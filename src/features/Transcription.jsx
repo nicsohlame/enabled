6cfb2d9e509e06ee763c25de9ebe6057544031cb
Justin <justin80@live.com.sg>
2025-06-17 14:03:51 +0800

    Implemented basic functionality of speech to text transcription without UI

--- src/features/Transcription.jsx
+++ src/features/Transcription.jsx
@@ -1,7 +1,33 @@
+import react, { useState, useEffect } from "react";
+import { AssemblyAI } from "assemblyai";
+
+
 function Transciption() {
+  const [text, setText] = useState("");
+  const client = new AssemblyAI({
+    apiKey: '182784a0e39145d196a34c10d1e47e8f',
+  })
+
+  const audioUrl = 'https://s3-us-west-2.amazonaws.com/rev.ai/resources/FTC_Sample_1.mp3';
+
+  const config = {
+    audio_url: audioUrl,
+  }
+
+  const run = async () => {
+    const transcript = await client.transcripts.create(config);
+    console.log(transcript.text);
+    setText(transcript.text);
+  }
+
+  useEffect(() => {
+    run();
+  }, []);
+
   return (
     <>
       <h1>Transciption</h1>
+      <p>{text}</p>
     </>
   );
 }
